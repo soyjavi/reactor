407bdb5625cf242a0574b9ae86a4ed4ad9befaa9
soyjavi <hello@soyjavi.com>
2019-11-28 12:23:52 +0700

    Use Hooks within <Layout>

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import { func, node } from 'prop-types';
-import React, { createContext, PureComponent } from 'react';
+import React, { useState, createContext } from 'react';
 import { View } from 'react-native';
 import styles from './Layout.style';
 
@@ -8,53 +8,40 @@ import { ENV, LAYOUT } from '../../common';
 const { IS_WEB, IS_SERVER, IS_MOBILE_WEB } = ENV;
 const { Provider, Consumer: LayoutConsumer } = createContext('reactor:layout');
 
-class LayoutView extends PureComponent {
-  static propTypes = {
-    children: node,
-    onLayout: func,
-  };
+const LayoutView = ({ children, onLayout, ...inherit }) => {
+  const [key, setKey] = useState(`${LAYOUT.VIEWPORT.W}x${LAYOUT.VIEWPORT.H}`);
 
-  static defaultProps = {
-    children: undefined,
-    onLayout() {},
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = { key: `${LAYOUT.VIEWPORT.W}x${LAYOUT.VIEWPORT.H}` };
-  }
-
-  _onLayout = ({ nativeEvent: { layout = {} } }) => {
-    const { props: { onLayout } } = this;
-
+  const handleLayout = ({ nativeEvent: { layout = {} } }) => {
     setTimeout(() => {
       LAYOUT.calc();
       onLayout(LAYOUT.VIEWPORT);
-      this.setState({ key: `${layout.width}x${layout.height}` });
+      setKey(`${layout.width}x${layout.height}`);
     }, 40);
-  }
+  };
 
-  render() {
-    const {
-      _onLayout,
-      props: { children, onLayout, ...inherit },
-      state: { key },
-    } = this;
+  return (
+    <Provider value={{ style: LAYOUT.STYLE, viewport: LAYOUT.VIEWPORT }}>
+      <View
+        {...inherit}
+        key={key}
+        onLayout={IS_WEB && !IS_SERVER && !IS_MOBILE_WEB ? handleLayout : undefined}
+        style={[styles.container, inherit.style]}
+      >
+        {children}
+      </View>
+    </Provider>
+  );
+};
 
-    return (
-      <Provider value={{ style: LAYOUT.STYLE, viewport: LAYOUT.VIEWPORT }}>
-        <View
-          {...inherit}
-          key={key}
-          onLayout={IS_WEB && !IS_SERVER && !IS_MOBILE_WEB ? _onLayout : undefined}
-          style={[styles.container, inherit.style]}
-        >
-          {children}
-        </View>
-      </Provider>
-    );
-  }
-}
+LayoutView.propTypes = {
+  children: node,
+  onLayout: func,
+};
+
+LayoutView.defaultProps = {
+  children: undefined,
+  onLayout() {},
+};
 
 export { LayoutConsumer };
 
