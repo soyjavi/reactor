d439c112dbb0c8215a7413113a6ccbd469e71124
soyjavi <hello@soyjavi.com>
2019-11-28 12:41:47 +0700

    Use Hooks within <Link> and <ProgressBar>

--- components/Link/Link.js
+++ components/Link/Link.js
@@ -1,37 +1,27 @@
 import { string } from 'prop-types';
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 
 import { THEME } from '../../common';
 import Text from '../Text';
 
-class Link extends PureComponent {
-  static propTypes = {
-    href: string.isRequired,
-  };
+const Link = ({ href, ...inherit }) => {
+  const [hover, setHover] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = { hover: false };
-  }
+  return (
+    <Text
+      {...inherit}
+      accessibilityRole="link"
+      href={href}
+      color={hover && !inherit.styleHover ? THEME.COLOR.ERROR : undefined}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      style={[inherit.style, hover && inherit.styleHover]}
+    />
+  );
+};
 
-  render() {
-    const {
-      props: { href, ...inherit },
-      state: { hover },
-    } = this;
-
-    return (
-      <Text
-        {...inherit}
-        accessibilityRole="link"
-        href={href}
-        color={hover && !inherit.styleHover ? THEME.COLOR.ERROR : undefined}
-        onMouseEnter={() => this.setState({ hover: true })}
-        onMouseLeave={() => this.setState({ hover: false })}
-        style={[inherit.style, hover && inherit.styleHover]}
-      />
-    );
-  }
-}
+Link.propTypes = {
+  href: string.isRequired,
+};
 
 export default Link;
--- components/ProgressBar/ProgressBar.js
+++ components/ProgressBar/ProgressBar.js
@@ -1,41 +1,31 @@
 import { number } from 'prop-types';
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 
 import Motion from '../Motion';
 import styles from './ProgressBar.style';
 
-class ProgressBar extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { layoutWidth: 0 };
-  }
+const ProgressBar = ({ progress = 0, ...inherit }) => {
+  const [layoutWidth, setLayoutWidth] = useState(0);
 
-  render() {
-    const {
-      props: { progress = 0, ...inherit },
-      state: { layoutWidth = 0 },
-    } = this;
-
-    return (
-      <View
-        onLayout={({ nativeEvent: { layout = {} } }) => this.setState({ layoutWidth: layout.width })}
-        style={[
-          styles.container,
-          inherit.style,
-          inherit.trackColor && { backgroundColor: inherit.trackColor },
-        ]}
-      >
-        <Motion
-          duration={inherit.duration}
-          style={[styles.progress, inherit.color && { backgroundColor: inherit.color }]}
-          timeline={[{ property: 'width', value: progress * layoutWidth }]}
-          type="timing"
-        />
-      </View>
-    );
-  }
-}
+  return (
+    <View
+      onLayout={({ nativeEvent: { layout = {} } }) => setLayoutWidth(layout.width)}
+      style={[
+        styles.container,
+        inherit.style,
+        inherit.trackColor && { backgroundColor: inherit.trackColor },
+      ]}
+    >
+      <Motion
+        duration={inherit.duration}
+        style={[styles.progress, inherit.color && { backgroundColor: inherit.color }]}
+        timeline={[{ property: 'width', value: progress * layoutWidth }]}
+        type="timing"
+      />
+    </View>
+  );
+};
 
 ProgressBar.propTypes = {
   progress: number,
