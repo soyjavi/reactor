f861e2a851b71e5e59a4a8d2df50846f36ac2ad0
soyjavi <hello@soyjavi.com>
2019-11-29 08:18:52 +0700

    Use pureFunction within <Viewport>

--- components/Viewport/Viewport.js
+++ components/Viewport/Viewport.js
@@ -1,7 +1,7 @@
 import {
   array, bool, func, node, number, object, oneOfType,
 } from 'prop-types';
-import React, { createElement, PureComponent } from 'react';
+import React, { createElement } from 'react';
 import { View, SafeAreaView, ScrollView } from 'react-native';
 
 import { ENV, LAYOUT, THEME } from '../../common';
@@ -11,68 +11,54 @@ import styles from './Viewport.style';
 const { MOTION } = THEME;
 const { IS_NATIVE } = ENV;
 
-class Viewport extends PureComponent {
-  static propTypes = {
-    backward: bool,
-    children: node,
-    onScroll: func,
-    scroll: bool,
-    styleContent: oneOfType([array, number, object]),
-    visible: bool,
-  };
-
-  static defaultProps = {
-    backward: false,
-    children: undefined,
-    onScroll: undefined,
-    scroll: true,
-    styleContent: [],
-    visible: true,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      height: IS_NATIVE ? LAYOUT.VIEWPORT.H : '100vh',
-      width: IS_NATIVE ? LAYOUT.VIEWPORT.W : '100vw',
-    };
-  }
-
-  _onScroll = ({ nativeEvent: { contentOffset: { y } } }) => {
-    const { props: { onScroll } } = this;
+const Viewport = ({
+  backward, children, onScroll, scroll, styleContent, visible, ...inherit
+}) => {
+  const handlenScroll = ({ nativeEvent: { contentOffset: { y } } }) => {
     onScroll({ y });
-  }
+  };
 
-  render() {
-    const {
-      _onScroll,
-      props: {
-        backward, children, onScroll, scroll, styleContent, visible, ...inherit
-      },
-      state: { height, width },
-    } = this;
+  const height = IS_NATIVE ? LAYOUT.VIEWPORT.H : '100vh';
+  const width = IS_NATIVE ? LAYOUT.VIEWPORT.W : '100vw';
+  const props = scroll && onScroll
+    ? { onScroll: handlenScroll, scrollEventThrottle: 32 }
+    : {};
 
-    return (
-      <Motion
-        duration={MOTION.DURATION}
-        style={[styles.container, { height, width }, inherit.style]}
-        timeline={backward && visible
-          ? [{ property: 'translateX', value: -64 }]
-          : [{ property: 'translateX', value: visible ? 0 : width }]}
-      >
-        <SafeAreaView style={styles.safeArea}>
-          { createElement(
-            scroll ? ScrollView : View,
-            {
-              ...(scroll && onScroll ? { onScroll: _onScroll, scrollEventThrottle: 32 } : {}),
-              style: [styles.content, styleContent],
-            },
-            children,
-          )}
-        </SafeAreaView>
-      </Motion>
-    );
-  }
-}
+  return (
+    <Motion
+      duration={MOTION.DURATION}
+      style={[styles.container, { height, width }, inherit.style]}
+      timeline={backward && visible
+        ? [{ property: 'translateX', value: -64 }]
+        : [{ property: 'translateX', value: visible ? 0 : width }]}
+    >
+      <SafeAreaView style={styles.safeArea}>
+        { createElement(
+          scroll ? ScrollView : View,
+          { ...props, style: [styles.content, styleContent] },
+          children,
+        )}
+      </SafeAreaView>
+    </Motion>
+  );
+};
+
+Viewport.propTypes = {
+  backward: bool,
+  children: node,
+  onScroll: func,
+  scroll: bool,
+  styleContent: oneOfType([array, number, object]),
+  visible: bool,
+};
+
+Viewport.defaultProps = {
+  backward: false,
+  children: undefined,
+  onScroll: undefined,
+  scroll: true,
+  styleContent: [],
+  visible: true,
+};
 
 export default Viewport;
