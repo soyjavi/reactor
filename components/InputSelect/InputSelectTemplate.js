f49e1e34250fd3bfbe4bda817187eab24d5e3d58
soyjavi <hello@soyjavi.com>
2019-11-29 08:11:16 +0700

    Stateless component

--- components/InputSelect/InputSelectTemplate.js
+++ components/InputSelect/InputSelectTemplate.js
@@ -1,48 +1,42 @@
 import { bool, string } from 'prop-types';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 
 import Text from '../Text';
 import { InputIcon } from '../Input';
 import styles from './InputSelectTemplate.style';
 
-class InputSelectTemplate extends PureComponent {
-  static propTypes = {
-    active: bool,
-    caption: string,
-    disabled: bool,
-    selected: bool,
-    title: string.isRequired,
-  };
+const InputSelectTemplate = ({
+  active, caption, disabled, title, selected, ...inherit
+}) => (
+  <View style={inherit.style}>
+    <InputIcon active={active || selected} />
+    <View style={styles.texts}>
+      <Text lighten={disabled} style={styles.title}>
+        {title}
+      </Text>
+      { caption && (
+        <Text caption level={2} lighten>
+          {caption}
+        </Text>
+      )}
+    </View>
+  </View>
+);
 
-  static defaultProps = {
-    active: false,
-    caption: undefined,
-    disabled: undefined,
-    selected: false,
-  };
+InputSelectTemplate.propTypes = {
+  active: bool,
+  caption: string,
+  disabled: bool,
+  selected: bool,
+  title: string.isRequired,
+};
 
-  render() {
-    const {
-      active, caption, disabled, title, selected, ...inherit
-    } = this.props;
-
-    return (
-      <View style={inherit.style}>
-        <InputIcon active={active || selected} />
-        <View style={styles.texts}>
-          <Text lighten={disabled} style={styles.title}>
-            {title}
-          </Text>
-          { caption && (
-            <Text caption level={2} lighten>
-              {caption}
-            </Text>
-          )}
-        </View>
-      </View>
-    );
-  }
-}
+InputSelectTemplate.defaultProps = {
+  active: false,
+  caption: undefined,
+  disabled: undefined,
+  selected: false,
+};
 
 export default InputSelectTemplate;
