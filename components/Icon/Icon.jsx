0ce2388660b9f2f53181fac6c9441050bf8afbca
JΛVI <hello@soyjavi.com>
2020-02-01 10:37:19 +0700

    Fixed <Icon> imports on native environments

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -1,3 +1,17 @@
+import {
+  AntDesign,
+  Entypo,
+  EvilIcons,
+  Feather,
+  FontAwesome,
+  Foundation,
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Octicons,
+  SimpleLineIcons,
+  Zocial,
+} from '@expo/vector-icons';
 import { number, string, oneOf } from 'prop-types';
 import React from 'react';
 
@@ -10,7 +24,20 @@ const DEFAULT = {
 };
 
 const Icon = ({ color = COLOR.TEXT, family = DEFAULT.FAMILY, size = SPACE.REGULAR, value = DEFAULT.VALUE }) => {
-  let IconFamily = require(`@expo/vector-icons/${family}`).default;
+  let IconFamily;
+
+  if (family === 'MaterialCommunityIcons') IconFamily = MaterialCommunityIcons;
+  else if (family === 'AntDesign') IconFamily = AntDesign;
+  else if (family === 'Entypo') IconFamily = Entypo;
+  else if (family === 'EvilIcons') IconFamily = EvilIcons;
+  else if (family === 'Feather') IconFamily = Feather;
+  else if (family === 'FontAwesome') IconFamily = FontAwesome;
+  else if (family === 'Foundation') IconFamily = Foundation;
+  else if (family === 'Ionicons') IconFamily = Ionicons;
+  else if (family === 'MaterialIcons') IconFamily = MaterialIcons;
+  else if (family === 'Octicons') IconFamily = Octicons;
+  else if (family === 'SimpleLineIcons') IconFamily = SimpleLineIcons;
+  else if (family === 'Zocial') IconFamily = Zocial;
 
   return <IconFamily name={value} color={color} size={size} />;
 };
@@ -24,7 +51,6 @@ Icon.propTypes = {
     'Feather',
     'FontAwesome',
     'Foundation',
-    'Icons',
     'Ionicons',
     'MaterialCommunityIcons',
     'MaterialIcons',
