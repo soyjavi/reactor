149f9c4d81738599aaf272034a8e09544afcdba8
JΛVI <hello@soyjavi.com>
2020-08-30 10:33:26 +0700

    Improved <Snackbar>

--- components/Snackbar/Snackbar.style.js
+++ components/Snackbar/Snackbar.style.js
@@ -1,26 +1,36 @@
 import { StyleSheet } from 'react-native';
 
 import { THEME } from '../../common';
+import { useEnvironment } from '../../hooks';
 
-const { COLOR, SPACE } = THEME;
+const { IS_WEB } = useEnvironment();
+const { SNACKBAR, SPACE } = THEME;
 
 export default StyleSheet.create({
   button: {
     alignSelf: 'center',
   },
 
+  container: {
+    position: IS_WEB ? 'fixed' : 'absolute',
+    width: '100%',
+    zIndex: 2,
+  },
+
+  content: SNACKBAR,
+
+  top: {
+    top: 0,
+  },
+
+  bottom: {
+    bottom: 0,
+  },
+
   caption: {
     display: 'flex',
     flexDirection: 'row',
     flex: 1,
-  },
-
-  dialog: {
-    padding: SPACE.M,
-  },
-
-  dialogOverlay: {
-    backgroundColor: COLOR.TRANSPARENT,
   },
 
   icon: {
